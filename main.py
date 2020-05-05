4f1b57621f40ddd1d181e1636ac3be09ee6fb102
Shikhar Sharma <shikharsharma@Shikhars-MacBook-Air.local>
2020-05-05 16:31:02 -0400

    added code to take care of all 3 workload files

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from rewards import Rewards
 
-dataPath = '../TrainData/preprocessed_benchmark_userclicks_14_19_10003_1000000_batch146_19_.csv'
-anomaly_strt_end = [(1529015043,1529016293),(1528999019,1528999419),(1529002179,1529003079),(1529007602,1529008102),
-                    (1529030089,1529031589),(1529023063,1529023763)]
+# path of the csv here
+dataPath_1 = '../TrainData/preprocessed_benchmark_userclicks_14_19_10003_1000000_batch146_19_.csv'
+dataPath_2 = '../TrainData/preprocessed_benchmark_userclicks_11_20_10003_1000000_batch146_17_.csv'
+dataPath_3 = '../TrainData/preprocessed_benchmark_userclicks_1_18_10000_1000000_batch146_20_.csv'
 
-reward_dict = Rewards(dataPath, anomaly_strt_end).get_rewards_dict()
+# copy corresponding anomalous intervals from the Excel file here
+anomaly_strt_end_1 = [(1529015043,1529016293),(1528999019,1528999419),(1529002179,1529003079),(1529007602,1529008102),
+                    (1529030089,1529031589),(1529023063,1529023763)]
+anomaly_strt_end_2 = [(1529015034,1529016293),(1528999019,1528999419),(1529002179,1529003079),(1529030089,1529031589),
+                      (1529007602,1529008102),(1529023063,1529023763)]
+anomaly_strt_end_3 = [(1529015034,1529016234),(1528999019,1528999419),(1529002179,1529003079),(1529007602,1529008102),
+                      (1529030089,1529031589),(1529023063,1529023763)]
+
+
+# get the rewards dictionary
+reward_dict = Rewards(dataPath_1, anomaly_strt_end_1).get_rewards_dict()
 
 print(reward_dict)
--- rewards.py
+++ rewards.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import numpy as np
 
-class Rewards():
+
+class Rewards:
 
     def __init__(self, dataPath, anomaly_strt_end):
 
+        # using the input start and end timestamps for anomalous periods
         self.anomaly_strt_end = anomaly_strt_end
+        # using the input path of the file
         self.dataPath = dataPath
+        # read csv into DataFrame
         self.df = pd.read_csv(dataPath)
         # dropping empty columns
         self.drop_empty_cols()
@@ -16,6 +20,14 @@ class Rewards():
         self.class_entropy = self.calc_class_entropy()
 
     def get_rewards_dict(self):
+        """
+
+        Call this method on the initialized object to get the sorted reward dictionary (no arguments required)
+
+        Returns:
+            A sorted dictionary containing rewards for each feature
+
+        """
         final_dict = {}
         j = 0
         for colName, _ in self.df.iteritems():
@@ -23,17 +35,31 @@ class Rewards():
             # if j ==7:
             #     break
             if colName not in ['t', 'anomalous']:
+
                 df_temp = self.df[[colName, 'anomalous']]
-                map_dict = dict(df_temp.groupby(by=[df_temp[colName], df_temp['anomalous']]).count().reset_index()[colName].value_counts())
+                map_dict = dict(df_temp.groupby(by=[df_temp[colName], df_temp['anomalous']]).count().reset_index()[
+                                    colName].value_counts())
                 df_temp['segment'] = df_temp.apply(lambda x: 2 if map_dict[x[colName]] > 1 else x['anomalous'], axis=1)
+
                 df_temp = df_temp.sort_values(by=[colName])
                 seg_entropy = self.calc_seg_entropy(df_temp)
+
                 reward = self.class_entropy / seg_entropy
                 final_dict.update({colName: reward})
-        final_dict = {k: v for k, v in sorted(final_dict.items(), key=lambda item: item[1], reverse = True)}
+        final_dict = {k: v for k, v in sorted(final_dict.items(), key=lambda item: item[1], reverse=True)}
         return final_dict
 
     def calc_seg_entropy(self, df):
+        """
+
+        Method used to calculate the total segmentation entropy for a single feature
+
+        Arguments:
+            df (Pandas DataFrame): DataFrame with 3 columns - feature, anomalous, segment
+        Returns:
+            entropy (float): The segmentation entropy for the feature
+
+        """
         x = list(df['segment'])
         count = 0
         seg = 1
@@ -81,24 +107,48 @@ class Rewards():
         entropy += penalty
         return entropy
 
-
     def drop_empty_cols(self):
+        """
+
+        Used to drop columns that have NA in all its rows
+        Method is run when the object is created
+
+        """
         drop_cols_list = []
         for colName, col in self.df.iteritems():
-            if col.isna().all() == True:
+            if col.isna().all():
                 drop_cols_list.append(colName)
         self.df = self.df.drop(columns=drop_cols_list)
 
     def create_class_col(self):
+        """
+
+        Used to create a column that identifies if a row belongs to the reference or the anomalous interval
+        Method is run when the object is created
+
+        """
+
         self.df['anomalous'] = self.df['t'].apply(lambda x: self.create_class_lambda(x))
 
     def create_class_lambda(self, col_val):
+        """
+
+        Method applied to each row of the DataFrame to get the 'anomalous' column
+
+        """
         for limits in self.anomaly_strt_end:
             if col_val >= limits[0] and col_val <= limits[1]:
                 return 1
         return 0
 
     def calc_class_entropy(self):
+        """
+
+        Used to calculate the class entropy
+        Method is run when the object is created
+
+        """
+
         p_i = self.df.anomalous.value_counts()[0] / len(self.df)
         p_j = self.df.anomalous.value_counts()[1] / len(self.df)
-        return  (-1 * p_i * np.log(p_i)) + (-1 * p_j * np.log(p_j))
+        return (-1 * p_i * np.log(p_i)) + (-1 * p_j * np.log(p_j))
